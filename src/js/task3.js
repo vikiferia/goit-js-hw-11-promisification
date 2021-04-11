71d8c352ee02a0ebe72172dbcd8c1c52a76e79ae
Viktoriia Fenenko <v.l.fenenko@gmail.com>
2021-04-11 17:06:12 +0300

    Исправления

--- src/js/task3.js
+++ src/js/task3.js
@@ -2,27 +2,29 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const makeTransaction = (transaction) => {
-  return new Promise((res, rej) => {
+const makeTransaction = transaction => {
+  return new Promise((resolve, reject) => {
     const delay = randomIntegerFromInterval(200, 500);
 
     setTimeout(() => {
       const canProcess = Math.random() > 0.3;
 
       if (canProcess) {
-        res(transaction.id, delay);
-      } else {
-        rej(transaction.id);
+        resolve({
+          id: transaction.id,
+          time: delay,
+        });
       }
+      reject(transaction.id);
     }, delay);
   });
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ({ id, time }) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
-const logError = (id) => {
+const logError = id => {
   console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 
